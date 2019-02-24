6c590663172eec3c42864248d578db5086edfeb1
Karen Neumann <karen@websavvystudio.com>
2019-02-24 13:20:04 -0500

    Change API url
    need to use ?_embed so we can access featured image for Dog.js

--- src/Dog.js
+++ src/Dog.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 export default class Dog extends React.Component {
-
-  render = () => {
-    const { dog } = this.props;
-    return (
-      <article key={dog.id} className="dog container">
-          <h2 dangerouslySetInnerHTML={{__html:dog.title.rendered}} />   
-          <p dangerouslySetInnerHTML={{__html:dog.content.rendered}} />
-          <hr></hr>
-    </article>
-    )
-  }
-
+	render = () => {
+		const { dog } = this.props;
+		// get featured image - size options: thumbnail, medium, medium_large, full
+		const image = dog._embedded['wp:featuredmedia']['0']['media_details']['sizes']['medium_large'].source_url;
+		// get alt text if none, use title +  text
+		const alt =
+			dog._embedded['wp:featuredmedia']['0']['alt_text'] || dog.title.rendered + ' - dog available for adoption';
+		console.log(image);
+		return (
+			<article key={dog.id} className="dog container">
+				<h2 dangerouslySetInnerHTML={{ __html: dog.title.rendered }} />
+				<img src={image} alt={alt} />
+				<p dangerouslySetInnerHTML={{ __html: dog.content.rendered }} />
+				<hr />
+			</article>
+			// need a back button
+		);
+	};
 }
--- src/Helpers.js
+++ src/Helpers.js
@@ -1,8 +1,12 @@
 // Consts and helpers functions
 
 // WordPress site URL
-const WP_URL = 'https://hackathonkarenandbrezo.wpcomstaging.com/wp-json/wp/v2/rescue_me_dogs';
+const WP_URL = 'https://hackathonkarenandbrezo.wpcomstaging.com/wp-json/wp/v2/rescue_me_dogs?_embed';
 // Metadata
 //TODO: Convert Array into array of objects to add display name for meta
-export const RESCUE_ME_META = ['rescue_me_meta_good_with_kids','rescue_me_meta_good_with_cats','rescue_me_meta_good_with_dogs'];
+export const RESCUE_ME_META = [
+	'rescue_me_meta_good_with_kids',
+	'rescue_me_meta_good_with_cats',
+	'rescue_me_meta_good_with_dogs'
+];
 export default WP_URL;
