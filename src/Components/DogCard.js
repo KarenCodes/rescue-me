6bfb5a0b8fe441532f1103275da9d25fc2633e27
Karen Neumann <karen@websavvystudio.com>
2019-03-01 13:27:19 -0500

    dog card click
    make entire dog card clickable & remove button

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -12,13 +12,11 @@ export default class DogCard extends React.Component {
 		return (
 			<li className="dog-card" key={dog.id}>
 				<article key={dog.id} className="dog container">
-					<Image dog={dog} size={'thumbnail'} />
-					<Name dog={dog} />
-					<Features dog={dog} />
-					<GoodWith dog={dog} />
-
-					<Link className="button-link" to={`/dog/${dog.slug}`}>
-						<button className="link-dog">More about {dog.title.rendered}</button>
+					<Link className="card-link" key={dog.id} to={`/dog/${dog.slug}`}>
+						<Image dog={dog} size={'thumbnail'} />
+						<Name dog={dog} />
+						<Features dog={dog} />
+						<GoodWith dog={dog} />
 					</Link>
 				</article>
 			</li>
