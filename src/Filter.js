242bf3b844a3c72415842c31abee8fd85aca7b5e
brezocordero <brezo.cordero@automattic.com>
2019-03-02 22:33:42 -0600

    Add Multiple filters and class when selected

--- src/Filter.js
+++ src/Filter.js
@@ -1,68 +1,79 @@
 import React from 'react';
-import { RESCUE_ME_META } from './Helpers.js';
+import { FILTERS } from './Helpers.js';
 
 const Button = props => {
-	return <button onClick={props.onClick}>{props.text}</button>;
+  return (
+    <button onClick={props.onClick} className={props.className}>
+      {props.text}
+    </button>
+  );
 };
-
+const isFilterSelected = (conditions, value) => {
+  for (let group in conditions) {
+    if (conditions[group].includes(value)) {
+      return true;
+    }
+  }
+  return false;
+};
+// Adds or removes a filter value to the list
+const toggleFilter = (props, group, value) => {
+  if (!props.conditions) return;
+  const newConditions = Object.assign(props.conditions);
+  // If filter group is not in conditions, add new group with value
+  if (!(group in newConditions)) newConditions[group] = new Array(value);
+  else {
+    // If group is in conditions, and value is in group, remove it.
+    if (newConditions[group].find(valueC => valueC === value)) {
+      newConditions[group] = newConditions[group].filter(
+        conditionsValue => conditionsValue !== value
+      );
+      // If there are no more values in group, remove group
+      if (newConditions[group].length === 0) delete newConditions[group];
+    }
+    // If group is in conditions, and value is not in group, add it.
+    else {
+      newConditions[group].push(value);
+    }
+  }
+  props.setConditions(newConditions);
+  return true;
+};
 export default class Filter extends React.Component {
-	// Function to get filters present in dogs, could be used for a tag cloud
-	getFilters = dogs => {
-		const filters = [];
-		// 1. For each meta in RESCUE_ME_META,
-		// 1.1 filter the dogs, get the number of dogs with the meta
-		// 2. Put filters in Local Storage
-		RESCUE_ME_META.map(key => {
-			const dogsWithMeta = dogs.filter(dog => {
-				return dog.meta[key] === true;
-			});
-			if (dogsWithMeta.length > 0)
-				filters.push({
-					name: key,
-					number: dogsWithMeta.length,
-					slug: key
-				});
-			return true;
-		});
-		return filters;
-	};
-
-	// Ads a condition to the list - This is unfinished
-	addCondition = (props, name, value) => {
-		if (!props.conditions) return;
-		const newConditions = Object.assign(props.conditions);
-		name in newConditions ? delete newConditions[name] : (newConditions[name] = value);
-		this.props.setConditions(newConditions);
-		return true;
-	};
-
-	// Function to display buttons with filters for dogs
-	getButtons = () => {
-		return (
-			//Buttons
-			<div>
-				<ul className="filters">
-					{RESCUE_ME_META.map(group => {
-						return (
-							<li key={group.name}>
-								<h2>{group.label}</h2>
-								<ul>
-									{group.values.map(value => (
-										<Button
-											key={value.name}
-											onClick={() => this.addCondition(this.props, group.name, value.name)}
-											text={value.label}
-										/>
-									))}
-								</ul>
-							</li>
-						);
-					})}
-				</ul>
-			</div>
-		);
-	};
-	render = () => {
-		return <div>{this.getButtons()}</div>;
-	};
+  // Function to display buttons with filters for dogs
+  getButtons = () => {
+    return (
+      //Buttons
+      <div>
+        <ul className="filters">
+          {FILTERS.map(group => {
+            return (
+              <li key={group.name}>
+                <h2>{group.label}</h2>
+                <ul>
+                  {group.values.map(value => (
+                    <Button
+                      key={value.name}
+                      className={
+                        isFilterSelected(this.props.conditions, value.name)
+                          ? 'selected'
+                          : ''
+                      }
+                      onClick={() =>
+                        toggleFilter(this.props, group.name, value.name)
+                      }
+                      text={value.label}
+                    />
+                  ))}
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  };
+  render = () => {
+    return <div>{this.getButtons()}</div>;
+  };
 }
