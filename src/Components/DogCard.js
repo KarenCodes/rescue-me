e2746372f54faf811766ca5c994b0840f03dcc0c
Karen Neumann <karen@websavvystudio.com>
2019-03-04 16:05:30 -0500

    Dog and DogCard don't need class
    changed them to functions

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -5,21 +5,19 @@ import Image from './Image';
 import Features from './Features';
 import GoodWith from './GoodWith';
 
-export default class DogCard extends React.Component {
-	render = () => {
-		const { dog } = this.props;
+const DogCard = ({ dog }) => {
+	return (
+		<li className="dog-card" key={dog.id}>
+			<article key={dog.id} className="doglist container">
+				<Link className="card-link" key={dog.id} to={`/dog/${dog.slug}`}>
+					<Image dog={dog} size={'medium'} />
+					<Name dog={dog} />
+					<Features dog={dog} />
+					<GoodWith dog={dog} />
+				</Link>
+			</article>
+		</li>
+	);
+};
 
-		return (
-			<li className="dog-card" key={dog.id}>
-				<article key={dog.id} className="doglist container">
-					<Link className="card-link" key={dog.id} to={`/dog/${dog.slug}`}>
-						<Image dog={dog} size={'medium'} />
-						<Name dog={dog} />
-						<Features dog={dog} />
-						<GoodWith dog={dog} />
-					</Link>
-				</article>
-			</li>
-		);
-	};
-}
+export default DogCard;
