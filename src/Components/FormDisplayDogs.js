2d579001e0da831cbcc17b06bc1435589ad29a02
Karen Neumann <karen@websavvystudio.com>
2019-03-11 19:17:58 -0400

    class or class less
    OLD is with class

--- src/Components/FormDisplayDogs.js
+++ src/Components/FormDisplayDogs.js
@@ -2,43 +2,28 @@ import React from 'react';
 import toggleFilter from '../Filter.js';
 import FilteredDogsList from '../FilteredDogsList';
 
-export default class FormDisplayDogs extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			// dogs: [],
-			conditions: {}
-		};
+const FormDisplayDogs = props => {
+	// build conditions based on form input
+	// props.house can have a value of "house" or "apartment" or null
+	// props.kids can have a value of "kids" or "no kids" or null
+
+	const conditions = {},
+		dogs = {};
+	if (props.kids === 'kids') {
+		// set good with kids condition
+		conditions['rescue_me_meta_good_with_kids'] = 'true';
+		// toggleFilter('', group, 'true');
+	}
+	if (props.house === 'apartment') {
+		//set small condition
+	} else {
+		//set all sizes
 	}
 
-	buildConditions = props => {
-		// build conditions based on form input
-		// props.house can have a value of "house" or "apartment" or null
-		// props.kids can have a value of "kids" or "no kids" or null
-		console.log(props);
-		if (props.kids === 'kids') {
-			// set good with kids condition
-			const group = 'rescue_me_meta_good_with_kids';
-			// toggleFilter('', group, 'true');
-		}
-		if (props.house === 'apartment') {
-			//set small condition
-		} else {
-			//set all sizes
-		}
-	};
-
-	render() {
-		this.buildConditions(this.props);
-		// return <FilteredDogsList conditions={this.state.conditions} />;
-		return (
-			<div>
-				<p>
-					list of filtered dogs, how to do this, created Components/FormDisplayDogs to set up conditions, but
-					I am lost: state, toggleFilter...
-				</p>
-				<p>also need to remove form from page?</p>
-			</div>
-		);
-	}
-}
+	return (
+		<div>
+			<FilteredDogsList dogs={dogs} conditions={conditions} />;
+		</div>
+	);
+};
+export default FormDisplayDogs;
