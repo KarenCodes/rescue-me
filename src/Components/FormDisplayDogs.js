e6a8442fef76d6579e8a0c0dd3207a5dc7de4a7e
Karen Neumann <karen@websavvystudio.com>
2019-03-11 16:42:11 -0400

    Commented out broken bits
    added description of what this needs to do

--- src/Components/FormDisplayDogs.js
+++ src/Components/FormDisplayDogs.js
@@ -1,15 +1,42 @@
 import React from 'react';
+import toggleFilter from '../Filter.js';
+import FilteredDogsList from '../FilteredDogsList';
 
-const FormDisplayDogs = props => {
-	// build conditions
-	console.log(props);
-	if (props.kids === 'kids') {
-		// set good with kids condition
+export default class FormDisplayDogs extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			// dogs: [],
+			conditions: {}
+		};
 	}
-	if (props.house === 'house') {
-		//set small condition
-	}
-	return <p>list matching dogs</p>;
-};
 
-export default FormDisplayDogs;
+	buildConditions = props => {
+		// build conditions based on form input
+		console.log(props);
+		if (props.kids === 'kids') {
+			// set good with kids condition
+			const group = 'rescue_me_meta_good_wih_kids';
+			// toggleFilter('', group, 'true');
+		}
+		if (props.house === 'apartment') {
+			//set small condition
+		} else {
+			//set all sizes
+		}
+	};
+
+	render() {
+		this.buildConditions(this.props);
+		// return <FilteredDogsList conditions={this.state.conditions} />;
+		return (
+			<div>
+				<p>
+					list of filtered dogs, how to do this, created Components/FormDisplayDogs to set up conditions, but
+					I am lost: state, toggleFilter...
+				</p>
+				<p>also need to remove form from page?</p>
+			</div>
+		);
+	}
+}
