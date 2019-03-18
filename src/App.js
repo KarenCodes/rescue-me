7e26b2b9be47d0d450d659255d75df764ca7a335
Karen Neumann <karen@websavvystudio.com>
2019-03-18 19:10:45 -0400

    Add a site footer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from './Main';
 import Header from './Header';
+import Footer from './Footer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Main />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<Main />
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = props => {
+	return (
+		<footer>
+			<p>&copy;2019 Site by Brezo Cordero and Karen Neumann</p>
+		</footer>
+	);
+};
+
+export default Footer;
