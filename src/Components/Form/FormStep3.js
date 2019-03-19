2aef5e997512d278fb5b36dada6c63892eb89193
Karen Neumann <karen@websavvystudio.com>
2019-03-19 18:01:24 -0400

    Form changes

--- src/Components/Form/FormStep3.js
+++ src/Components/Form/FormStep3.js
@@ -5,54 +5,52 @@ const FormStep3 = props => {
 		return null;
 	}
 	const { kids, house, username } = props;
-	const message = !house ? 'homeless' : house;
+	const message = house ? ` at your ${house}` : 'wherever you live';
 
 	return (
-		<React.Fragment>
-			<fieldset>
-				<legend>Will your dog be with kids?</legend>
-				<p>
-					Nice, {message}. Are there any mini-versions of {username ? username : 'you'} running around?
-				</p>
-				<div className="form-group">
-					<div className="form-item">
-						<label htmlFor="kids">
-							<span className="emoji" role="img" aria-label="Yes kids">
-								👶
-							</span>
-							<span className="text">Kids</span>
-						</label>
-						<input
-							className="form-control"
-							id="kids"
-							name="kids"
-							type="radio"
-							checked={kids === 'kids' ? true : false}
-							value="kids"
-							onChange={props.handleChange}
-						/>
-					</div>
-
-					<div className="form-item">
-						<label htmlFor="no-kids">
-							<span className="emoji" role="img" aria-label="No kids">
-								👎
-							</span>
-							<span className="text">No Kids</span>
-						</label>
-						<input
-							className="form-control"
-							id="no-kids"
-							name="kids"
-							type="radio"
-							checked={kids === 'no kids' ? true : false}
-							value="no kids"
-							onChange={props.handleChange}
-						/>
-					</div>
+		<fieldset>
+			<legend>Will your dog be with kids?</legend>
+			<p>
+				Are there any mini-versions of {username ? username : 'you'} (or other kids) {message}?
+			</p>
+			<div className="form-group">
+				<div className="form-item">
+					<label htmlFor="kids">
+						<span className="emoji" role="img" aria-label="Yes kids">
+							👶
+						</span>
+						<span className="text">Kids</span>
+					</label>
+					<input
+						className="form-control"
+						id="kids"
+						name="kids"
+						type="radio"
+						checked={kids === 'kids' ? true : false}
+						value="kids"
+						onChange={props.handleChange}
+					/>
 				</div>
-			</fieldset>
-		</React.Fragment>
+
+				<div className="form-item">
+					<label htmlFor="no-kids">
+						<span className="emoji" role="img" aria-label="No kids">
+							👎
+						</span>
+						<span className="text">No Kids</span>
+					</label>
+					<input
+						className="form-control"
+						id="no-kids"
+						name="kids"
+						type="radio"
+						checked={kids === 'no kids' ? true : false}
+						value="no kids"
+						onChange={props.handleChange}
+					/>
+				</div>
+			</div>
+		</fieldset>
 	);
 };
 
